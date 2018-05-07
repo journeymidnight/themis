861f7784456357d0fa17a4d05439383ef0f8c54b
baicheng <bch.fan@haihangyun.com>
2018-05-07 13:11:12 +0800

    checkout serf leader down
    Change-Id: Ied1f0664ba46b0a791d9415458f02ff8b506816f
    Jira: 0S-880
    Type: NF

--- monitor/event.go
+++ monitor/event.go
@@ -2,6 +2,7 @@ package monitor
 
 import (
 	"github.com/ljjjustin/themis/config"
+	"errors"
 )
 
 type Event struct {
@@ -48,8 +49,48 @@ func (c *EventCollector) Start() error {
 func (c *EventCollector) DrainEvents() (Events, error) {
 	select {
 	case events := <-c.EventChan:
+		if judgeSerfDown(events) {
+			return nil, errors.New("leader serf down")
+		}
 		return events, nil
 	default:
 		return nil, nil
 	}
+}
+
+func judgeSerfDown(events Events) bool {
+
+	// group by network type
+	netType := map[string][]string{}
+
+	for _, e := range events {
+
+		t := netType[e.NetworkTag]
+
+		if t == nil {
+			t := []string{}
+			netType[e.NetworkTag] = t
+		}
+
+		netType[e.NetworkTag] = append(netType[e.NetworkTag], e.Status)
+
+	}
+
+	for _, statusList := range netType {
+
+		failedTimes := 0
+
+		for _, ststus := range statusList {
+
+			if ststus == "failed" {
+				failedTimes += 1
+			}
+		}
+
+		if failedTimes >= len(statusList) - 1 {
+			return true
+		}
+	}
+
+	return false
 }
